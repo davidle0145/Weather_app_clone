27dbd58e614d6612a77249cf20b3168ddd663e01
LE THANH DAT <ledat10b8@gmail.com>
2023-07-14 17:45:26 +0700

    final

---
+++ assets/js/api.js
@@ -0,0 +1,48 @@
+/**
+ * @license MIT
+ * @fileoverview All api related stuff like api_key, api request etc.
+ * @copyright lethanhdat 2023 All rights reserved
+ * @author lethanhdat <ledat10b8@gmail.com>
+ */
+
+'use strict';
+
+const api_key = "5aebf3b29a2f632e6d340f9ecf048451";
+
+/**
+ * Fetch data from server
+ * @param {string} URL API url 
+ * @param {Function} callback  callback
+ */
+
+export const fetchData = function(URL, callback) {
+    fetch(`${URL}&appid=${api_key}`)
+        .then(res => res.json())
+        .then(data => callback(data))
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeocoding(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * @param {string} query Search query e.g.: "Ha Noi", "Ho Chi Minh City"
+     * @returns 
+     */
+    geocoding(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
+
+
+
+
